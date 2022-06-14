e77caf97eb3677ea44b64e2c6e04bb04e2d50a16
omerfruk <omer.fruk3547@gmail.com>
2022-06-14 17:57:38 +0300

    Bismillah

---
+++ model/user.go
@@ -0,0 +1,11 @@
+package model
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name"`
+	Surname  string `json:"surname"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
