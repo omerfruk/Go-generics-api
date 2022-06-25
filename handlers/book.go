fb132bc446013a213dbcc8506ee52c0960031f36
omerfruk <omer.fruk3547@gmail.com>
2022-06-25 16:16:22 +0300

    New : Book handlers eklendi

---
+++ handlers/book.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/omerfruk/Go-generics-api/model"
+	"github.com/omerfruk/Go-generics-api/services"
+)
+
+type BookHandler struct {
+	BaseHandler[model.Book]
+	bookService services.BookService
+}
+
+func NewBookHandler(s services.BookService) BookHandler {
+	return BookHandler{
+		BaseHandler: BaseHandler[model.Book]{
+			BaseService: s,
+		},
+		bookService: s,
+	}
+}
+
+func (b *BookHandler) GetBookByAuthor(c *fiber.Ctx) error {
+	author := c.Params("author")
+	book, err := b.bookService.GetBookByAuthor(author)
+	if err != nil {
+		return c.Status(500).JSON(err)
+	}
+	return c.JSON(book)
+}
