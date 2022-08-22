aadfc9aabb93ca473d5596065dee1f911005594c
omerfruk <omer.fruk3547@gmail.com>
2022-08-22 21:37:53 +0300

    update : update put methodu olarak değiştirildi

--- router/routers.go
+++ router/routers.go
@@ -20,14 +20,14 @@ func Setup(app fiber.Router) {
 	app.Get("/users", userHandler.GetAll)
 	app.Get("/users/:id", userHandler.GetByID)
 	app.Post("/users", userHandler.Create)
-	app.Post("/users/:id", userHandler.Update)
+	app.Put("/users/:id", userHandler.Update)
 	app.Delete("/users/:id", userHandler.Delete)
 	app.Get("/users/email/:email", userHandler.GetByEmail)
 
 	app.Get("/books", bookHandler.GetAll)
 	app.Get("/books/:id", bookHandler.GetByID)
 	app.Post("/books", bookHandler.Create)
-	app.Post("/books/:id", bookHandler.Update)
+	app.Put("/books/:id", bookHandler.Update)
 	app.Delete("/books/:id", bookHandler.Delete)
 	app.Get("/books/author/:author", bookHandler.GetBookByAuthor)
 }
